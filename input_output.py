cfd7d29ed59a15c55547afdcc1f07c61e1bdbd61
ArmanIslam3307 <armanislam3307@gmail.com>
2026-05-13 13:15:33 +0600

    input Output file

---
+++ input_output.py
@@ -0,0 +1,17 @@
+name = "Arman"
+age = 30
+print("Name:", name) #Concatanation 
+print(f"Name: {name} \nAge: {age}") #F String
+
+
+# --------Input------
+your_name = input("Your Name: ")
+print(your_name)
+
+num1 = input("Enter Num1: ")
+num2 = input("Enter Num2: ")
+result = num1 + num2
+
+print(result, type(result)) #Input always returns string
+
+
