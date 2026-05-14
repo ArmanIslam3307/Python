1142c3106f2d04146ba5a7cf94bef80aa72b2a07
ArmanIslam3307 <armanislam3307@gmail.com>
2026-05-14 12:34:24 +0600

    Added Condition

---
+++ condition.py
@@ -0,0 +1,24 @@
+age = int(input("Enter Your Age: "))
+
+if age >= 18:
+    print("You Can vote!")
+else:
+    print("Sorry You Can't Vote!")
+    
+    
+num1 = int(input("Enter Number: "))
+
+if num1 < 0:
+    print("Negative")
+else:
+    print("Positive")
+    
+    
+num2 = int(input("Number 2: "))
+
+if num2 % 2 == 1:
+    print("Odd")
+else:
+    print("Even")
+    
+    
--- operator.py
+++ operator.py
@@ -1,24 +1,51 @@
-num1 = int(input("Enter Fitst Number"))
-num2 = int(input("Enter Second Number"))
+# num1 = int(input("Enter Fitst Number"))
+# num2 = int(input("Enter Second Number"))
 
-add = num1 + num2
-print(add)
+# # ============= Arithmetic Operators ===============
+# add = num1 + num2
+# print(add)
 
-sub = num1 - num2
-print(sub)
+# sub = num1 - num2
+# print(sub)
 
-multi = num1 * num2
-print(multi)
+# multi = num1 * num2
+# print(multi)
 
-div = num1 / num2
-print(div)
+# div = num1 / num2
+# print(div)
 
-floor_res = num1 // num2
-print(floor_res)
+# floor_res = num1 // num2
+# print(floor_res)
 
-mod = num1 % num2
-print(mod)
+# mod = num1 % num2
+# print(mod)
 
-pow = num1 ** num2
-print(pow)
+# pow = num1 ** num2
+# print(pow)
 
+# =================== Assignment Operators =====================
+
+num3 = int(input("Enter Number 3: "))
+num4 = int(input("Enter Number 4: "))
+
+num3 += num4
+add_assign = num3
+print(num3)
+print(add_assign)
+
+num3-=num4
+print(num3)
+add_sub = num3
+print(add_sub)
+
+# ========= Bitwise Operator (For Binary Calculations) =======
+bit_a = 5 #101
+bit_b = 7 #111
+bit_c = 10
+bit_d = 3
+
+print("BItwise and: ",bit_a & bit_b)
+print("BItwise or: ",bit_a | bit_b)
+print("BItwise Xor: ",bit_c ^ bit_d)
+print("BItwise Left Shift: ",5 << 3)
+print("BItwise Right Shift: ",bit_a >> bit_b)
