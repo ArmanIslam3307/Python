f976cd4da9982f46ac93c88c427e2423aa2efa33
ArmanIslam3307 <armanislam3307@gmail.com>
2026-05-16 08:54:03 +0600

    Added some files and make some changes

--- condition.py
+++ condition.py
@@ -1,24 +1,66 @@
-age = int(input("Enter Your Age: "))
+# # ====== if else  =====
 
-if age >= 18:
-    print("You Can vote!")
-else:
-    print("Sorry You Can't Vote!")
-    
-    
-num1 = int(input("Enter Number: "))
+# age = int(input("Enter Your Age: "))
 
-if num1 < 0:
-    print("Negative")
-else:
-    print("Positive")
+# if age >= 18:
+#     print("You Can vote!")
+# else:
+#     print("Sorry You Can't Vote!")
     
-    
-num2 = int(input("Number 2: "))
+# # ========= Check Negative or Positive Number ================
+# num1 = int(input("Enter Number: "))
 
-if num2 % 2 == 1:
-    print("Odd")
+# if num1 < 0:
+#     print("Negative")
+# elif num1 > 0:
+#     print("Positive")
+# else:
+#     print("Zero")
+    
+    
+# num2 = int(input("Number 2: "))
+
+# if num2 % 2 == 1:
+#     print("Odd")
+# elif num2 == 0:
+#     print("Zero")
+# else:
+#     print("Even")
+    
+    
+
+# marks = int(input("Enter Your Marks: "))
+
+# if marks >= 80:
+#     print("A+")
+# elif marks >= 70:
+#     print("A")
+# elif marks >= 60:
+#     print("A-")
+# elif marks >= 50:
+#     print("B")
+# elif marks >= 40:
+#     print("D")
+# else:
+#     print("F")
+    
+
+# -------Nested If ------------
+result = input("p or f")
+if result == "p":
+    marks = int(input("Enter Your Marks: "))
+
+    if marks >= 80:
+        print("A+")
+    elif marks >= 70:
+        print("A")
+    elif marks >= 60:
+        print("A-")
+    elif marks >= 50:
+        print("B")
+    elif marks >= 40:
+        print("D")
+    else:
+        print("F")
 else:
-    print("Even")
-    
-    
+    print("Fail")
--- input_output.py
+++ input_output.py
@@ -1,63 +1,36 @@
-name = "Arman"
-age = 30
-print("Name:", name) #Concatanation 
-print(f"Name: {name} \nAge: {age}") #F String
+# name = "Arman"
+# age = 30
+# print("Name:", name) #Concatanation 
+# print(f"Name: {name} \nAge: {age}") #F String
 
 
-# --------Input------
-your_name = input("Your Name: ")
-print(your_name)
+# # --------Input------
+# your_name = input("Your Name: ")
+# print(your_name)
 
-num1 = int(input("Enter Num1: ")) #Type Casting
-num2 = int(input("Enter Num2: ")) #Type Casting
+# num1 = int(input("Enter Num1: ")) #Type Casting
+# num2 = int(input("Enter Num2: ")) #Type Casting
 
-result = num1 + num2
+# result = num1 + num2
 
-print(result, type(result)) #Input always returns string
-
-
-# Assignment - 2
-# Write a program to input student name & marks of 3 subjects. 
-# Print name & percentage in output. 
-
-student_name = input("Enter your name: ")
-bangla_marks = input("Enter bangla Marks: ")
-maths_marks = input("Enter Maths Marks: ")
-science_marks = input("Enter Science Marks: ") 
-
-# calcualating percentage 
-percentage = ((int(bangla_marks) + int(maths_marks) + int(science_marks))/300)*100
-
-# # print result 
-print(f"The result of {student_name} is {int(percentage)}%. Well done!!")
-
-# optimized solution
-print("Percentage Calculator")
-student_name2 = input("Enter your name: ")
-bangla_marks2 = int(input("Enter bangla Marks: "))
-maths_marks2 = int(input("Enter Maths Marks: "))
-science_marks2 = int(input("Enter Science Marks: "))
-
-# # calcualating percentage 
-percentage = ((bangla_marks2 + maths_marks2 + science_marks2)/300)*100
-
-# # print result 
-print(f"The result of {student_name} is {int(percentage)}%. Well done!!")
+# print(result, type(result)) #Input always returns string
 
 
-# Q2: Write a program that collects multiple types of data to store in a dictionary 
-# and print output.
+# # multple input from user 
+# # input from user to add two number and print result
+# x = input("Enter first number: ")
+# y = input("Enter second number: ") 
+# print(f"Sum of {x} & {y} is {int(x) + int(y)}")
 
-#  initializing a dictionary
-user_data = {} 
+# HW: write a program to input student name and marks of 3 subjects. 
+# And print name and percentage in output.   
 
-# input from user
-user_data['name'] = input("Enter your name: ")
-user_data['age'] = int(input("Enter your age: "))
-user_data['height'] = float(input("Enter your height: "))
-user_data['student'] = input("Are you a student (yes/no)")
+student_name = input("Enter Your Name: ")
 
-# print the input from user
-print(user_data)
+bangla = input("Bangla Marks: ")
+english = input("English Marks: ")
+math = input("Math Marks: ")
 
+average = (bangla + english + math) / 3
 
+print(f"{student_name} got {average}%")5
